8774ca4e7ca37fa7c6033dfb6b9b9457e5fbafc5
MrSmollett <93076884+MrSmollett@users.noreply.github.com>
2022-02-17 22:47:50 +0300

    Delete usersDB.php

--- usersDB.php
+++
@@ -1,5 +0,0 @@
-<?php 
-				return [ ['login' => 'admin', 'password' => 'P@ssw0rd'], 
-										 ['login' => 'moderator', 'password' => 'password'], 
-										 ['login' => 'user', 'password' => '123'], 
-];
